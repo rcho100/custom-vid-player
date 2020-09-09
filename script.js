e2dacf80d969af9af8ce178be8a7668a2b28b8dc
Richard Cho <richardcho100@gmail.com>
2020-09-09 01:19:08 -0400

    Enable play & pause of video by clicking on screen

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ const play = document.getElementById('play')
 const stop = document.getElementById('stop')
 const progress = document.getElementById('progress')
 const timestamp = document.getElementById('timestamp')
+
+// Play & pause (toggle) video
+function toggleVideoStatus() {
+    if (video.paused) {
+        video.play()
+    } else {
+        video.pause()
+    }
+}
+
+
+//Event listeners
+video.addEventListener('click', toggleVideoStatus)
+
